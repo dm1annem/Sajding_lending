683f14c2e809a6d74c20e9d0a2721cbe661ec451
Нуфтанчики <300225@mail.ru>
2020-04-28 00:06:23 +0300

    added a logo

--- src/components/sajding-main-wrapper.vue
+++ src/components/sajding-main-wrapper.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="sajding-main-wrapper"> 
     <p>{{ msg }}</p>
+    <sajding-nav/>
     
     </div>
 </template>
 
 <script>
+import sajdingNav from './sajding-nav'
+
 export default {
     name: 'sajding-main-wrapper',
+    components: {
+       sajdingNav, 
+    },
     data() {
         return {
             msg: 'Я главная обёртка'
--- src/components/sajding-nav.vue
+++ src/components/sajding-nav.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="sajding-nav">
-
+       <img :src=" require('../assets/img/LOGO-SAYDING.png') "> 
+       <img>
     </div>
 </template>
 
@@ -9,7 +10,7 @@ export default {
     name: 'sajding-nav',
     data(){
 
-    }
+    },
 }
 </script>
 
