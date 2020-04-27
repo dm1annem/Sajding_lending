df2dee355382e22c6e75c7756fb1f26f4510c39c
Нуфтанчики <300225@mail.ru>
2020-04-27 23:07:10 +0300

    created sajding-nav

---
+++ src/components/sajding-nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="sajding-nav">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'sajding-nav',
+    data(){
+
+    }
+}
+</script>
+
+<style>
+
+</style>
